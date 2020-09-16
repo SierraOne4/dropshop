3a4b48e5d0e56e2384fd775beda1b195fbeefa86
pwhisper <yinanwang@sierra.one>
2020-09-15 22:09:54 -0400

    Update query.php

--- docs/query.php
+++ docs/query.php
@@ -1,20 +1,10 @@
-<html>
-<head>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-<script>
-function urlBuilder(swyftUrl) {
-     //swyftUrl = swyftUrl.match(/(?<=sys:swyft&track:).*(?=&org)/); 
-      //just use raw for now, we don't have proper QR codes.
-     return "https://script.google.com/macros/s/AKfycbwLjD2nxs62zkaLQz7PV51TrriftHY_U3uM99WUJei4uHoDx2AV/exec?track="+swyftUrl+"&mode=QUERY";
-    }
-	
-	window.onload = function () {
-	 var s = document.createElement("script");
-            s.type = "text/javascript";
-            s.src = urlBuilder(swyftUrl);
-            $("head").append(s);
-	
-	}
-</script>
-</html>
+<?php
+$uid = $_POST['User name'];
+$type = $_POST['Question: TYPE'];
+$code = $_POST['Barcode'];
+$timestamp = $_POST['Timestamp (Scanned)'];
+
+echo $uid." ".$type." ".$code." ".$timestamp;
+
+
+?>
